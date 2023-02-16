84d84b065998d107f6aa6eb49c13e7fe6d19ea95
AnkitGourav <Ankitgourav035@gmail.com>
2023-02-17 05:28:49 +0530

    Main.java: Print usage when no file is supplied.

--- app/src/main/java/cpp/to/java/Main.java
+++ app/src/main/java/cpp/to/java/Main.java
@@ -1,12 +1,36 @@
 package cpp.to.java;
 
+import java.net.URISyntaxException;
+
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) { return; }
+        if (args.length == 0) {
+            usageHelp();
+            return;
+        }
 
         if (!args[0].substring(args[0].lastIndexOf('.')).equals(".cpp")) {
             invalidFileType(args[0]);
         }
+    }
+
+    private static void usageHelp() {
+        String jarFileName = "";
+        try {
+            jarFileName = new String(Main.class
+                    .getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .toURI()
+                    .getPath()
+            );
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print("Usage: ");
+        System.out.print(jarFileName.substring(jarFileName.lastIndexOf('/') + 1));
+        System.out.println(" <fileName>.cpp");
     }
 
     private static void invalidFileType(String fileName) {
