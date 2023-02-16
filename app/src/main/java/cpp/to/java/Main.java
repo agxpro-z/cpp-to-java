9518ff479adae7080962495d4c322b5b6e26a3d2
AnkitGourav <Ankitgourav035@gmail.com>
2023-02-17 05:11:59 +0530

    Main.java: only support .cpp file
    Other file types will be treated as invalid file type.

--- app/src/main/java/cpp/to/java/Main.java
+++ app/src/main/java/cpp/to/java/Main.java
@@ -2,5 +2,14 @@ package cpp.to.java;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) { return; }
+
+        if (!args[0].substring(args[0].lastIndexOf('.')).equals(".cpp")) {
+            invalidFileType(args[0]);
+        }
+    }
+
+    private static void invalidFileType(String fileName) {
+        System.out.println("Invalid file type: " + fileName);
     }
 }
