87b4e2ee6d2c41d749731b6617725e5537f86ce9
AnkitGourav <Ankitgourav035@gmail.com>
2023-02-17 05:55:11 +0530

    Main.java: Add midway exit method.

--- app/src/main/java/cpp/to/java/Main.java
+++ app/src/main/java/cpp/to/java/Main.java
@@ -10,9 +10,22 @@ public class Main {
         }
 
         if (!args[0].substring(args[0].lastIndexOf('.')).equals(".cpp")) {
-            invalidFileType(args[0]);
+            exit("Invalid file type: " + args[0]);
         }
     }
+
+    /*
+     * Exit method to exit program in between
+     *
+     * @param message = message to print before exit.
+     */
+    public static void exit(String message) {
+        if (message.length() == 0) { System.exit(0); }
+
+        System.err.println(message);
+        System.exit(1);
+    }
+
 
     private static void usageHelp() {
         String jarFileName = "";
@@ -31,9 +44,5 @@ public class Main {
         System.out.print("Usage: ");
         System.out.print(jarFileName.substring(jarFileName.lastIndexOf('/') + 1));
         System.out.println(" <fileName>.cpp");
-    }
-
-    private static void invalidFileType(String fileName) {
-        System.out.println("Invalid file type: " + fileName);
     }
 }
