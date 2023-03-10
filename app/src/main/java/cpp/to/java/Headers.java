de3e469b8d133ca1f31f1353c7c1a38c6d59c2b0
AnkitGourav <Ankitgourav035@gmail.com>
2023-03-11 01:24:25 +0530

    Headers: Fix: Don't return empty lines if no headers are present

--- app/src/main/java/cpp/to/java/Headers.java
+++ app/src/main/java/cpp/to/java/Headers.java
@@ -6,6 +6,10 @@ public class Headers {
     public static ArrayList<String> getJavaHeaders(ArrayList<String> headers) {
         ArrayList<String> javaHeaders = new ArrayList<>();
 
+        // Return empty list if no headers are present
+        if (headers.size() == 0)
+            return javaHeaders;
+
         for (String s : headers) {
             if (s.contains("iostream")) {
                 javaHeaders.add("import java.util.Scanner;\n");
